3dba5b2079cf35d33bdef07a7482e1683191617e
Felipe Bojorquez <felipe@febog.com>
2023-06-26 21:25:59 -0700

    Add initial Organization model.

---
+++ ConselvaBudget/Models/Organization.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConselvaBudget.Models
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "OrganizationCode")]
+        public int Code { get; set; }
+
+        [Display(Name = "Name")]
+        [StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
