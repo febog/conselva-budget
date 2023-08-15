e8c300d4194b4ce32f427d6156b3827003812ca1
Felipe Bojorquez <felipe@febog.com>
2023-08-14 23:18:30 -0700

    Update Administration area index.

--- ConselvaBudget/Areas/Administration/Pages/Index.cshtml.cs
+++ ConselvaBudget/Areas/Administration/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@ using ConselvaBudget.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using static ConselvaBudget.Areas.Administration.Pages.IndexModel;
 
 namespace ConselvaBudget.Areas.Administration.Pages
 {
@@ -15,7 +16,7 @@ namespace ConselvaBudget.Areas.Administration.Pages
         }
 
         public IList<BusinessProgram> BusinessPrograms { get; set; } = default!;
-        public IList<BusinessSubprogram> BusinessSubprograms { get; set; } = default!;
+        public IList<BusinessSubprogramVM> BusinessSubprograms { get; set; } = default!;
         public IList<AccountCategory> AccountCategories { get; set; } = default!;
         public IList<Project> Projects { get; set; } = default!;
 
@@ -26,14 +27,30 @@ namespace ConselvaBudget.Areas.Administration.Pages
                 _context.AccountCategories != null &&
                 _context.Projects != null)
             {
-                BusinessPrograms = await _context.BusinessPrograms.ToListAsync();
+                BusinessPrograms = await _context.BusinessPrograms
+                    .OrderBy(p => p.Code)
+                    .ToListAsync();
                 BusinessSubprograms = await _context.BusinessSubprograms
                     .Include(c => c.BusinessProgram)
                     .AsNoTracking()
+                    .Select(s => new BusinessSubprogramVM
+                    {
+                        Id = s.Id,
+                        Subprogram = s.Code + " - " + s.Name,
+                        Program = s.BusinessProgram.Code + " - " + s.BusinessProgram.Name
+                    })
+                    .OrderBy(s => s.Program)
+                    .ThenBy(s => s.Subprogram)
                     .ToListAsync();
                 AccountCategories = await _context.AccountCategories.ToListAsync();
                 Projects = await _context.Projects.ToListAsync();
             }
+        }
+        public class BusinessSubprogramVM
+        {
+            public int Id { get; set; }
+            public string Subprogram { get; set; }
+            public string Program { get; set; }
         }
     }
 }
