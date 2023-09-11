0e95b3ef24e3a6af9efc0009da312eb5b01c71e9
Felipe Bojorquez <felipe@febog.com>
2023-09-10 23:20:18 -0700

    Add Result totals.

--- ConselvaBudget/Models/Result.cs
+++ ConselvaBudget/Models/Result.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConselvaBudget.Models
 {
@@ -13,6 +14,21 @@ namespace ConselvaBudget.Models
         [StringLength(255)]
         [Required]
         public string Name { get; set; }
+
+        [Display(Name = "Total budget for result")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [ValidateNever]
+        public decimal ResultBudget => Activities.Sum(e => e.ActivityBudget);
+
+        [Display(Name = "Total expenses for result")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [ValidateNever]
+        public decimal ResultExpenses => Activities.Sum(e => e.ActivityExpenses);
+
+        [Display(Name = "Remaining budget for result")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [ValidateNever]
+        public decimal ResultRemainder => Activities.Sum(e => e.ActivityRemainder);
 
         [Display(Name = "Project")]
         public Project Project { get; set; }
