cc265a7be67de513cc1e6850f3748e3fff48a7e2
Felipe Bojorquez <felipe@febog.com>
2023-09-11 01:13:26 -0700

    Add Project totals.

--- ConselvaBudget/Models/Project.cs
+++ ConselvaBudget/Models/Project.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace ConselvaBudget.Models
 {
@@ -32,6 +33,21 @@ namespace ConselvaBudget.Models
         public string Comments { get; set; }
 
         public string DisplayName => $"{Segment} - {Name} ({ShortName})";
+
+        [Display(Name = "Total budget for project")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [ValidateNever]
+        public decimal ProjectBudget => Results.Sum(e => e.ResultBudget);
+
+        [Display(Name = "Total expenses for project")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [ValidateNever]
+        public decimal ProjectExpenses => Results.Sum(e => e.ResultExpenses);
+
+        [Display(Name = "Remaining budget for project")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [ValidateNever]
+        public decimal ProjectRemainder => Results.Sum(e => e.ResultRemainder);
 
         public virtual ICollection<Result> Results { get; set; }
     }
