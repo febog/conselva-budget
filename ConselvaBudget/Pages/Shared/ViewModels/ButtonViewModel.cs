4c83174301a085ca7a667e29155cd9be8e996318
Felipe Bojorquez <felipe@febog.com>
2023-11-29 01:17:08 -0800

    Create reusable Edit and Delete buttons. In the spirit of keeping things DRY, I have created reusable markup via Partial Views. These partial views contain the button markup so I don't have to repeat it every time I need a button. These partial views are strongly typed, so I created a new ViewModel folder to add the type and added said namespace to the Areas' _ViewImports file so that pages can find it. The type contains the parameters needed to use the anchor tag helper that exists underneath. I added these reusable buttons to the Manage page to start.

---
+++ ConselvaBudget/Pages/Shared/ViewModels/ButtonViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ConselvaBudget.Pages.Shared.ViewModels
+{
+    public class ButtonViewModel
+    {
+        public string AspPage { get; set; }
+        public int RouteId { get; set; }
+    }
+}
