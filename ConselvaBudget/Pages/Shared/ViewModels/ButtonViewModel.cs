8ecf15fd425a12a10593c96937a4f337e3a54113
Felipe Bojorquez <felipe@febog.com>
2023-12-04 14:43:31 -0800

    Remove old Button Partial Views.

--- ConselvaBudget/Pages/Shared/ViewModels/ButtonViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConselvaBudget.Pages.Shared.ViewModels
-{
-    public class ButtonViewModel
-    {
-        public string AspPage { get; set; }
-        public int RouteId { get; set; }
-    }
-}
