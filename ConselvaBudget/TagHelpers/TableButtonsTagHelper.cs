291957a89842c74bde937e92598dcc10924a0f58
Felipe Bojorquez <felipe@febog.com>
2023-12-04 12:29:06 -0800

    Author my custom Tag Helper for table action buttons. This allows me to keep things DRY and reuse markup. Followed tutorial in https://www.learnrazorpages.com/advanced/custom-tag-helpers

---
+++ ConselvaBudget/TagHelpers/TableButtonsTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Build.Framework;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class TableButtonsTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            // These arranges the buttons side by side using Bootstrap's flexbox utilities.
+            output.Attributes.Add("class", "d-flex gap-1 justify-content-center");
+        }
+    }
+}
