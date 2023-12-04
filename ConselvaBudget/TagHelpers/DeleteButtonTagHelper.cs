da1f742484e0d6a9a7f47cf322eb5f72a7c3d291
Felipe Bojorquez <felipe@febog.com>
2023-12-04 12:22:14 -0800

    Implement custom Tag Helpers for buttons. Instead of using Partial Views as implemented in a previous commit, use Tag Helpers instead to implement the reusable markup. Makes more sense for small, reusable sections of markup like buttons. No model binding needed. I wanted the functionality of the existing Anchor Tag Helper, so my custom Tag Helpers extend it as shown in https://softdevpractice.com/blog/asp-net-core-extending-built-tag-helpers/

---
+++ ConselvaBudget/TagHelpers/DeleteButtonTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class DeleteButtonTagHelper : AnchorTagHelper
+    {
+        public DeleteButtonTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            output.TagName = "a";
+
+            var a = new TagBuilder("a");
+            a.Attributes.Add("title", "Delete");
+            a.Attributes.Add("aria-label", "Delete");
+            a.Attributes.Add("class", "btn btn-danger btn-sm");
+            output.MergeAttributes(a);
+
+            output.Content.SetHtmlContent(@"<i class=""bi bi-trash3""></i>");
+        }
+    }
+}
---
+++ ConselvaBudget/TagHelpers/EditButtonTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class EditButtonTagHelper : AnchorTagHelper
+    {
+        public EditButtonTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            output.TagName = "a";
+
+            var a = new TagBuilder("a");
+            a.Attributes.Add("title", "Edit");
+            a.Attributes.Add("aria-label", "Edit");
+            a.Attributes.Add("class", "btn btn-secondary btn-sm");
+            output.MergeAttributes(a);
+
+            output.Content.SetHtmlContent(@"<i class=""bi bi-pencil-square""></i>");
+        }
+    }
+}
