8f6e0a14fe8a057bfab637fad63c858b2e170297
Felipe Bojorquez <felipe@febog.com>
2023-12-05 00:44:10 -0800

    Add Account Equivalents management.

---
+++ ConselvaBudget/Areas/Administration/Pages/Equivalents/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using ConselvaBudget.Data;
+using ConselvaBudget.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConselvaBudget.Areas.Administration.Pages.Equivalents
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ConselvaBudgetContext _context;
+
+        public IndexModel(ConselvaBudgetContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Donor> Donors { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Donors = await _context.Donors
+                .Include(d => d.EquivalentAccounts)
+                .OrderBy(d => d.Name)
+                .ToListAsync();
+        }
+    }
+}
