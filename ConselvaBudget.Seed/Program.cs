e9cec3d9cecfe57c8e464810df8ea43123b7616b
Felipe Bojorquez <felipe@febog.com>
2024-01-23 21:55:33 -0800

    Create separate Seed application. Adds a new C# console app project to the solution that will only handle database seeding. I will keep this separate from the rest of the main application code, since this only runs once at the beginning. This new project contains only the minimum to function: The EFCore package to create a DBContext options object and a reference to the main project to reuse the main application's model and DBContext. As to prevent seed data in the repo, the new data file is added to the .gitignore.

---
+++ ConselvaBudget.Seed/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConselvaBudget.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConselvaBudget.Seed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting database seed process...");
+
+            var options = new DbContextOptionsBuilder<ConselvaBudgetContext>()
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ConselvaBudgetContext-e30d848b-d770-45f5-aecc-8efa6536e102;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
+
+            Data.Initialize(options);
+
+            Console.WriteLine("Database seeded!");
+        }
+    }
+}
