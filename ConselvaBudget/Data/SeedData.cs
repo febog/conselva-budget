2c01d974e7b5bc8b2f041414f9dc384627eda882
Felipe Bojorquez <felipe@febog.com>
2024-01-26 15:32:50 -0800

    Add SeedData file to repo. Since business data has been moved out of the main project and into the separate ConselvaBudget.Seed app, SeedData.cs now only contains the seeding logic for the application's roles. This contains no business data and thus can be checked-in into the repo. It creates the roles used by the app authorization and thus needed when deploying the app.

---
+++ ConselvaBudget/Data/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using ConselvaBudget.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace ConselvaBudget.Data
+{
+    public static class SeedData
+    {
+        public static async Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            await SeedRolesAsync(serviceProvider);
+        }
+
+        private static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            using (var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>())
+            {
+                if (!await roleManager.RoleExistsAsync(Roles.Administrator))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(Roles.Administrator));
+                }
+
+                if (!await roleManager.RoleExistsAsync(Roles.Management))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(Roles.Management));
+                }
+
+                if (!await roleManager.RoleExistsAsync(Roles.Employee))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(Roles.Employee));
+                }
+
+                // Add roles for development
+                using (var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>())
+                {
+                    // Development email is stored in the Secret Manager,
+                    // which is accessed via the configuration service.
+                    var configService = serviceProvider.GetRequiredService<IConfiguration>();
+                    var developmentEmail = configService["Development:DevUserAccountEmail"];
+                    var user = await userManager.FindByNameAsync(developmentEmail ?? "");
+                    if (user != null)
+                    {
+                        if (!await userManager.IsInRoleAsync(user, Roles.Administrator))
+                        {
+                            await userManager.AddToRoleAsync(user, Roles.Administrator);
+                        }
+
+                        if (!await userManager.IsInRoleAsync(user, Roles.Management))
+                        {
+                            await userManager.AddToRoleAsync(user, Roles.Management);
+                        }
+
+                        if (!await userManager.IsInRoleAsync(user, Roles.Employee))
+                        {
+                            await userManager.AddToRoleAsync(user, Roles.Employee);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
