cc33ff275e9cc8589c562c7f0286fbcf623d9e87
Felipe Bojorquez <felipe@febog.com>
2024-01-29 00:21:20 -0800

    Exclude database seed from non-debug environments.

--- ConselvaBudget.Seed/Program.cs
+++ ConselvaBudget.Seed/Program.cs
@@ -9,10 +9,12 @@ namespace ConselvaBudget.Seed
         {
             Console.WriteLine("Starting database seed process...");
 
+#if DEBUG
             var options = new DbContextOptionsBuilder<ConselvaBudgetContext>()
                 .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ConselvaBudgetContext-e30d848b-d770-45f5-aecc-8efa6536e102;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
 
             Data.Initialize(options);
+#endif
 
             Console.WriteLine("Database seeded!");
         }
