f62538d1a2347fdafceea3218dfbf1581a0a508c
Felipe Bojorquez <felipe@febog.com>
2024-02-05 15:49:23 -0800

    Add skip message to database seed program.

--- ConselvaBudget.Seed/Program.cs
+++ ConselvaBudget.Seed/Program.cs
@@ -14,9 +14,11 @@ namespace ConselvaBudget.Seed
                 .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ConselvaBudgetContext-e30d848b-d770-45f5-aecc-8efa6536e102;Trusted_Connection=True;MultipleActiveResultSets=true").Options;
 
             Data.Initialize(options);
-#endif
 
             Console.WriteLine("Database seeded!");
+#else
+            Console.WriteLine("Skipping database seed in non-debug environments.");
+#endif
         }
     }
 }
