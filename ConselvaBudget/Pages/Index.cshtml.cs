1a27a2fac8d55a9254b61924f9f499622893e392
Felipe Bojorquez <felipe@febog.com>
2024-02-07 22:53:32 -0800

    Support culture selection. Adds a small form that calls a named Razor Page handler to set the culture cookie. https://learn.microsoft.com/en-us/aspnet/core/razor-pages/?view=aspnetcore-8.0&tabs=visual-studio#multiple-handlers-per-page https://learn.microsoft.com/en-us/aspnet/core/fundamentals/localization/select-language-culture?view=aspnetcore-8.0#set-the-culture-programmatically

--- ConselvaBudget/Pages/Index.cshtml.cs
+++ ConselvaBudget/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ConselvaBudget.Pages
@@ -15,6 +16,15 @@ namespace ConselvaBudget.Pages
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPostSetCulture(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
