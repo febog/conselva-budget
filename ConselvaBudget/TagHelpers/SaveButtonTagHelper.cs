7792dbf5044824a64bb25b36103f47efb2619778
Felipe Bojorquez <felipe@febog.com>
2024-02-27 00:14:37 -0800

    Create Save button tag helper.

---
+++ ConselvaBudget/TagHelpers/SaveButtonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class SaveButtonTagHelper : TagHelper
+    {
+        private readonly IStringLocalizer<Resources.SharedResource> _localizer;
+
+        public SaveButtonTagHelper(IStringLocalizer<Resources.SharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "input";
+            output.Attributes.Add("class", "btn btn-primary");
+            output.Attributes.Add("type", "submit");
+            output.Attributes.Add("value", _localizer["BUTTON_SAVE"]);
+        }
+    }
+}
