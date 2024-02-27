377a09bd6847b070a05d00edd4263abacdbd80f4
Felipe Bojorquez <felipe@febog.com>
2024-02-27 00:21:21 -0800

    Add Delete confirm button tag helper.

---
+++ ConselvaBudget/TagHelpers/DeleteConfirmButtonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class DeleteConfirmButtonTagHelper : TagHelper
+    {
+        private readonly IStringLocalizer<Resources.SharedResource> _localizer;
+
+        public DeleteConfirmButtonTagHelper(IStringLocalizer<Resources.SharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "input";
+            output.Attributes.Add("class", "btn btn-danger");
+            output.Attributes.Add("type", "submit");
+            output.Attributes.Add("value", _localizer["BUTTON_DELETE"]);
+        }
+    }
+}
