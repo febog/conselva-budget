7d33931bbcca0b0c92f435cec82f8b85032812e5
Felipe Bojorquez <felipe@febog.com>
2024-02-26 23:58:59 -0800

    Add Create button as a tag helper and localize it.

---
+++ ConselvaBudget/TagHelpers/CreateButtonTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class CreateButtonTagHelper : TagHelper
+    {
+        private readonly IStringLocalizer<Resources.SharedResource> _localizer;
+
+        public CreateButtonTagHelper(IStringLocalizer<Resources.SharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "input";
+            output.Attributes.Add("class", "btn btn-primary");
+            output.Attributes.Add("type", "submit");
+            output.Attributes.Add("value", _localizer["BUTTON_CREATE"]);
+        }
+    }
+}
