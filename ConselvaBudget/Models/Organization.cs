671e64a85161b12e55dd381f3033c9db1b73dd4f
Felipe Bojorquez <felipe@febog.com>
2024-02-29 20:01:33 -0800

    Add localization to Organization model.

--- ConselvaBudget/Models/Organization.cs
+++ ConselvaBudget/Models/Organization.cs
@@ -6,19 +6,19 @@ namespace ConselvaBudget.Models
     {
         public int Id { get; set; }
 
-        [Display(Name = "Code")]
+        [Display(Name = "ORGANIZATION_CODE")]
         [StringLength(10)]
         [Required]
         public string Code { get; set; }
 
-        [Display(Name = "Name")]
+        [Display(Name = "ORGANIZATION_NAME")]
         [StringLength(255)]
         [Required]
         public string Name { get; set; }
 
         public string DisplayName => $"{Code} - {Name}";
 
-        [Display(Name = "Accounts")]
+        [Display(Name = "ORGANIZATION_ACCOUNT_ASSIGNMENTS")]
         public virtual ICollection<AccountAssignment> AccountAssignments { get; set; }
     }
 }
