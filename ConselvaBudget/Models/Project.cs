616ffa854871d75aa18e3d4b2fe14d92aaf354b1
Felipe Bojorquez <felipe@febog.com>
2024-02-29 20:21:20 -0800

    Add localization to Project model.

--- ConselvaBudget/Models/Project.cs
+++ ConselvaBudget/Models/Project.cs
@@ -8,62 +8,61 @@ namespace ConselvaBudget.Models
     {
         public int Id { get; set; }
 
-        [Display(Name = "Donor")]
         public int DonorId { get; set; }
 
-        [Display(Name = "Name")]
+        [Display(Name = "PROJECT_NAME")]
         [StringLength(255)]
         [Required]
         public string Name { get; set; }
 
-        [Display(Name = "ShortName")]
+        [Display(Name = "PROJECT_SHORT_NAME")]
         [StringLength(255)]
         [Required]
         public string ShortName { get; set; }
 
-        [Display(Name = "Segment")]
+        [Display(Name = "PROJECT_SEGMENT")]
         public int Segment { get; set; }
 
-        [Display(Name = "Start date")]
+        [Display(Name = "PROJECT_START_DATE")]
         [DataType(DataType.Date)]
         public DateTime? StartDate { get; set; }
 
-        [Display(Name = "End date")]
+        [Display(Name = "PROJECT_END_DATE")]
         [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
 
-        [Display(Name = "Description")]
+        [Display(Name = "PROJECT_DESCRIPTION")]
         [StringLength(255)]
         public string Description { get; set; }
 
         public string DisplayName => $"{Segment} - {Name} ({ShortName})";
 
-        [Display(Name = "Total budget for project")]
+        [Display(Name = "PROJECT_TOTAL_BUDGET")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         [ValidateNever]
-        public decimal ProjectBudget => Results.Sum(e => e.ResultBudget);
+        public decimal TotalBudget => Results.Sum(e => e.ResultBudget);
 
-        [Display(Name = "Total expenses for project")]
+        [Display(Name = "PROJECT_TOTAL_EXPENSES")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         [ValidateNever]
         public decimal TotalExpenses => Results.Sum(e => e.ResultExpenses);
 
-        [Display(Name = "Remaining budget for project")]
+        [Display(Name = "PROJECT_REMAINING_BUDGET")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         [ValidateNever]
-        public decimal RemainingBudget => ProjectBudget - TotalExpenses;
+        public decimal RemainingBudget => TotalBudget - TotalExpenses;
 
-        [Display(Name = "Total deposits")]
+        [Display(Name = "PROJECT_TOTAL_DEPOSITS")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         [ValidateNever]
         public decimal TotalDeposits => Deposits.Sum(d => d.Amount);
 
-        [Display(Name = "Pending to deposit")]
+        [Display(Name = "PROJECT_PENDING_TO_DEPOSIT")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         [ValidateNever]
-        public decimal PendingToDeposit => ProjectBudget - TotalDeposits;
+        public decimal PendingToDeposit => TotalBudget - TotalDeposits;
 
-        [Display(Name = "Remaining in bank")]
+        [Display(Name = "PROJECT_REMAINING_IN_BANK")]
         [DisplayFormat(DataFormatString = "{0:C}")]
         [ValidateNever]
         public decimal RemainingInBank => TotalDeposits - TotalExpenses;
@@ -71,11 +70,11 @@ namespace ConselvaBudget.Models
         /// <summary>
         /// A Project is considered active if it has a Start and End date and we are in between those dates.
         /// </summary>
-        [Display(Name = "Active")]
+        [Display(Name = "PROJECT_IS_ACTIVE")]
         [ValidateNever]
         public bool IsActive => StartDate != null && EndDate != null && DateTime.Now > StartDate && DateTime.Now < EndDate;
 
-        [Display(Name = "Donor")]
+        [Display(Name = "PROJECT_DONOR")]
         public Donor Donor { get; set; }
 
         public virtual ICollection<Result> Results { get; set; }
