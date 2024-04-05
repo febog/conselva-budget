4574d610cf88e45031e8d7dc0d2bc524bfaddb83
Felipe Bojorquez <felipe@febog.com>
2024-04-04 17:55:39 -0700

    Add initial Request Delete logic.

---
+++ ConselvaBudget/Areas/Spending/Pages/Requests/Delete.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ConselvaBudget.Data;
+using ConselvaBudget.Models;
+
+namespace ConselvaBudget.Areas.Spending.Pages.Requests
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ConselvaBudgetContext _context;
+
+        public DeleteModel(ConselvaBudgetContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ExpensesRequest ExpensesRequest { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ExpensesRequest = await _context.SpendingRequests.FindAsync(id);
+
+            if (ExpensesRequest == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expensesRequest = await _context.SpendingRequests.FindAsync(id);
+
+            if (expensesRequest != null)
+            {
+                ExpensesRequest = expensesRequest;
+                _context.SpendingRequests.Remove(ExpensesRequest);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
