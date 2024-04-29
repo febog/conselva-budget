db6d3d2c8d6f657b412845683bd092abde849713
Felipe Bojorquez <felipe@febog.com>
2024-04-29 15:05:21 -0700

    Remove unused column

--- ConselvaBudget/Models/NotificationRecipient.cs
+++ ConselvaBudget/Models/NotificationRecipient.cs
@@ -10,12 +10,6 @@ namespace ConselvaBudget.Models
         [Required]
         public string AspNetUserId { get; set; }
 
-        [Display(Name = "Username")]
-        [StringLength(256)]
-        [Required]
-        [ValidateNever]
-        public string UsernameEmail { get; set; }
-
         [Display(Name = "Notification email address")]
         [StringLength(256)]
         [Required]
