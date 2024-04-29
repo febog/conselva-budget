47bf6b5c23a2e2266aaa287ed823210077692a25
Felipe Bojorquez <felipe@febog.com>
2024-04-29 14:42:03 -0700

    Create a table to store email notification addresses

---
+++ ConselvaBudget/Models/NotificationRecipient.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConselvaBudget.Models
+{
+    public class NotificationRecipient
+    {
+        [Key]
+        [StringLength(450)]
+        [Required]
+        public string AspNetUserId { get; set; }
+
+        [Display(Name = "Username")]
+        [StringLength(256)]
+        [Required]
+        [ValidateNever]
+        public string UsernameEmial { get; set; }
+
+        [Display(Name = "Notification email address")]
+        [StringLength(256)]
+        [Required]
+        [ValidateNever]
+        public string NotificationEmail { get; set; }
+    }
+}
