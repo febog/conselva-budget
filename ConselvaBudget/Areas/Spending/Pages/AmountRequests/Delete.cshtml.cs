40990c231d1c5ed2cd70e385e637cedcda305cf1
Felipe Bojorquez <felipe@febog.com>
2024-06-03 16:49:20 -0700

    Add initial scaffolding for AmountRequest

---
+++ ConselvaBudget/Areas/Spending/Pages/AmountRequests/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ConselvaBudget.Data;
+using ConselvaBudget.Models;
+
+namespace ConselvaBudget.Areas.Spending.Pages.AmountRequests
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ConselvaBudget.Data.ConselvaBudgetContext _context;
+
+        public DeleteModel(ConselvaBudget.Data.ConselvaBudgetContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AmountRequest AmountRequest { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var amountrequest = await _context.AmountRequests.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (amountrequest == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                AmountRequest = amountrequest;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var amountrequest = await _context.AmountRequests.FindAsync(id);
+            if (amountrequest != null)
+            {
+                AmountRequest = amountrequest;
+                _context.AmountRequests.Remove(AmountRequest);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ConselvaBudget/Areas/Spending/Pages/AmountRequests/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConselvaBudget.Data;
+using ConselvaBudget.Models;
+
+namespace ConselvaBudget.Areas.Spending.Pages.AmountRequests
+{
+    public class EditModel : PageModel
+    {
+        private readonly ConselvaBudget.Data.ConselvaBudgetContext _context;
+
+        public EditModel(ConselvaBudget.Data.ConselvaBudgetContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AmountRequest AmountRequest { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var amountrequest =  await _context.AmountRequests.FirstOrDefaultAsync(m => m.Id == id);
+            if (amountrequest == null)
+            {
+                return NotFound();
+            }
+            AmountRequest = amountrequest;
+           ViewData["ActivityBudgetId"] = new SelectList(_context.ActivityBudgets, "Id", "Id");
+           ViewData["RequestId"] = new SelectList(_context.Requests, "Id", "Description");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(AmountRequest).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AmountRequestExists(AmountRequest.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool AmountRequestExists(int id)
+        {
+            return _context.AmountRequests.Any(e => e.Id == id);
+        }
+    }
+}
