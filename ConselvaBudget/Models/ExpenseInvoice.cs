358f5c04dad0bebc520db4acda0d1e9f9d3eebd1
Felipe Bojorquez <felipe@febog.com>
2024-07-24 16:10:18 -0700

    Add localization to ExpenseInvoice and PaymentMethods

--- ConselvaBudget/Models/ExpenseInvoice.cs
+++ ConselvaBudget/Models/ExpenseInvoice.cs
@@ -9,6 +9,7 @@ namespace ConselvaBudget.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "EXPENSE_INVOICE_ACTIVITY_BUDGET_ID")]
         public int ActivityBudgetId { get; set; }
 
         public int RequestId { get; set; }
