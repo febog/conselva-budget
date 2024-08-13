dae4637f48c14fff1f05357a8e3d9bce710d26f8
Felipe Bojorquez <felipe@febog.com>
2024-08-12 20:51:00 -0700

    Add IsPaid column to Request

---
+++ ConselvaBudget/Data/ConselvaMigrations/20240813035013_AddPaidFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConselvaBudget.Data.ConselvaMigrations
+{
+    /// <inheritdoc />
+    public partial class AddPaidFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "Requests",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "Requests");
+        }
+    }
+}
