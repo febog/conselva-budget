88c37e6d38de392ed6354af9ff7cb6ad0244fa4d
Felipe Bojorquez <felipe@febog.com>
2025-05-21 18:42:36 -0700

    Add Expenses report model

---
+++ ConselvaBudget/Areas/Reporting/Pages/Expenses/Index.cshtml.cs
@@ -0,0 +1,59 @@
+using ConselvaBudget.Data;
+using ConselvaBudget.Models;
+using ConselvaBudget.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConselvaBudget.Areas.Reporting.Pages.Expenses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ConselvaBudgetContext _context;
+        private readonly IReportService _reportService;
+
+        public IndexModel(ConselvaBudgetContext context, IReportService reportService)
+        {
+            _context = context;
+            _reportService = reportService;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var expenses = await _context.ExpenseInvoices
+                .Include(e => e.ActivityBudget.Activity.Result.Project.Donor)
+                .Include(e => e.ActivityBudget.AccountAssignment.Organization)
+                .Include(e => e.ActivityBudget.AccountAssignment.Account)
+                .ToListAsync();
+
+            // Map to report view model
+            var reportData = MapExpensesReportData(expenses);
+
+            // Generate Excel file download
+            return _reportService.GenerateExcelFileDownload<ExpensesReportViewModel>(reportData);
+        }
+
+        private IList<ExpensesReportViewModel> MapExpensesReportData(IList<ExpenseInvoice> expenses)
+        {
+            var data = new List<ExpensesReportViewModel>();
+            foreach (var expense in expenses)
+            {
+                data.Add(new ExpensesReportViewModel
+                {
+                    Donor = expense.ActivityBudget.Activity.Result.Project.Donor.ShortName,
+                    Project = expense.ActivityBudget.Activity.Result.Project.Name,
+                    Result = expense.ActivityBudget.Activity.Result.Code,
+                    Activity = expense.ActivityBudget.Activity.Code,
+                    Program = expense.ActivityBudget.AccountAssignment.Organization.Name,
+                    Account = expense.ActivityBudget.AccountAssignment.DisplayName,
+                    ExpenseDate = expense.InvoiceDate.ToString("yyyy-MM-dd"),
+                    Vendor = expense.Vendor,
+                    InvoiceNumber = expense.InvoiceNumber,
+                    RequestId = expense.RequestId,
+                    TotalSpentAmount = expense.TotalSpentAmount,
+                });
+            }
+            return data;
+        }
+    }
+}
