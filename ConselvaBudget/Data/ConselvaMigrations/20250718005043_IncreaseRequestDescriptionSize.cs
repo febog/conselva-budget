dbc04f02ea1788de2ac827f0d94d4f8d7e2f15fb
Felipe Bojorquez <felipe@febog.com>
2025-07-17 17:53:35 -0700

    Increase Request description size (#67)
    - Doubles the field size for the Description in the Request.
    - Adds appropriate DB migration.
    - First migration under the new 9 version.

---
+++ ConselvaBudget/Data/ConselvaMigrations/20250718005043_IncreaseRequestDescriptionSize.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConselvaBudget.Data.ConselvaMigrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseRequestDescriptionSize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Requests",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Requests",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512);
+        }
+    }
+}
