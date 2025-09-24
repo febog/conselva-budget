d837ec98039ecb0abcb9dd1917d5420690098eb7
Felipe Bojorquez <felipe@febog.com>
2025-09-23 21:50:06 -0700

    Increase Request Description size (#94)
    Increase the request description size to 1024.

---
+++ ConselvaBudget/Data/ConselvaMigrations/20250924044838_IncreaseRequestDescription.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConselvaBudget.Data.ConselvaMigrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseRequestDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Requests",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Requests",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024);
+        }
+    }
+}
