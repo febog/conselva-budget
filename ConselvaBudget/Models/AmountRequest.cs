789a7b59a956c233568d15e6f8148aa72e112b4b
Felipe Bojorquez <felipe@febog.com>
2025-10-08 16:47:22 -0700

    Fix `AmountRequest` views (#107)
    - Adds the appropriate layout to the `AmountRequest` pages.
    - Adds localization.
    - Fixes navigation from `AmountRequest` pages.
    
    Fixes #98

--- ConselvaBudget/Models/AmountRequest.cs
+++ ConselvaBudget/Models/AmountRequest.cs
@@ -9,18 +9,17 @@ namespace ConselvaBudget.Models
     {
         public int Id { get; set; }
 
-        [Display(Name = "Budget")]
+        [Display(Name = "AMOUNT_REQUEST_ACTIVITY_BUDGET")]
         public int ActivityBudgetId { get; set; }
 
-        [Display(Name = "Request")]
         public int RequestId { get; set; }
 
-        [Display(Name = "Description")]
+        [Display(Name = "AMOUNT_REQUEST_DESCRIPTION")]
         [StringLength(512)]
         [Required]
         public string Description { get; set; }
 
-        [Display(Name = "Amount")]
+        [Display(Name = "AMOUNT_REQUEST_AMOUNT")]
         [Column(TypeName = "money")]
         [DataType(DataType.Currency)]
         public decimal Amount { get; set; }
@@ -35,17 +34,13 @@ namespace ConselvaBudget.Models
         [ValidateNever]
         public string ModifiedByUserId { get; set; }
 
-        [Display(Name = "CreatedDate")]
         public DateTime CreatedDate { get; set; }
 
-        [Display(Name = "ModifiedDate")]
         public DateTime ModifiedDate { get; set; }
 
-        [Display(Name = "Account")]
         [DeleteBehavior(DeleteBehavior.Restrict)]
         public ActivityBudget ActivityBudget { get; set; }
 
-        [Display(Name = "Request")]
         [DeleteBehavior(DeleteBehavior.Restrict)]
         public Request Request { get; set; }
     }
