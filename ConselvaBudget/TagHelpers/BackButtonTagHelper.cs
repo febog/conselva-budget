57689054317c79b85466d5dc52d3c333969565b8
Felipe Bojorquez <felipe@febog.com>
2025-10-09 17:31:30 -0700

    Add a Back button tag helper (#109)
    - Adds a back button tag helper similar to the existing ones.
    - It also adds the required localized strings for the button.
    - Inspired on the existing cancel button.

---
+++ ConselvaBudget/TagHelpers/BackButtonTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Localization;
+
+namespace ConselvaBudget.TagHelpers
+{
+    public class BackButtonTagHelper : AnchorTagHelper
+    {
+        private readonly IStringLocalizer<Resources.SharedResource> _localizer;
+
+        public BackButtonTagHelper(IHtmlGenerator generator,
+                                   IStringLocalizer<Resources.SharedResource> localizer) : base(generator)
+        {
+            _localizer = localizer;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            var label = _localizer["BUTTON_BACK"];
+
+            output.TagName = "a";
+
+            var a = new TagBuilder("a");
+            a.Attributes.Add("class", "btn btn-secondary");
+            output.MergeAttributes(a);
+
+            output.Content.SetHtmlContent(label);
+        }
+    }
+}
