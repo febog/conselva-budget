8d32147c7731a0855b844b7333b41836ad642d0e
Felipe Bojorquez <felipe@febog.com>
2025-10-10 18:22:43 -0700

    Add Credit Card view model and strings

---
+++ ConselvaBudget/Areas/Spending/Pages/Requests/CreditCardSubtotalsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConselvaBudget.Areas.Spending.Pages.Requests
+{
+    public class CreditCardSubtotalsViewModel
+    {
+        [Display(Name = "CREDIT_CARD_ISSUING_BANK")]
+        public string CreditCardIssuingBank { get; set; }
+
+        [Display(Name = "CREDIT_CARD_ENDING")]
+        public int CreditCardEnding { get; set; }
+
+        [Display(Name = "CREDIT_CARD_AMOUNT")]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+    }
+}
