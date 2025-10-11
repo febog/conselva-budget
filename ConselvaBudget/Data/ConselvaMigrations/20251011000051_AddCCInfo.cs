d4c3a8baad1e128227028eadc90ec0c274470b83
Felipe Bojorquez <felipe@febog.com>
2025-10-10 17:10:06 -0700

    Add CC info DB migration

---
+++ ConselvaBudget/Data/ConselvaMigrations/20251011000051_AddCCInfo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConselvaBudget.Data.ConselvaMigrations
+{
+    /// <inheritdoc />
+    public partial class AddCCInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreditCardEnding",
+                table: "ExpenseInvoices",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreditCardIssuingBank",
+                table: "ExpenseInvoices",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreditCardEnding",
+                table: "ExpenseInvoices");
+
+            migrationBuilder.DropColumn(
+                name: "CreditCardIssuingBank",
+                table: "ExpenseInvoices");
+        }
+    }
+}
