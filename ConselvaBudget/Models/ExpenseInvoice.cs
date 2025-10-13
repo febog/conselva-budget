0687024e223bc3ad349aea4310c08b95927cb8ed
Felipe Bojorquez <felipe@febog.com>
2025-10-13 12:34:42 -0700

    Fix CC ending type (#116)
    The correct data type for storing this information is `string`, since we
    want to be able to store leading zeroes in this number, not just the
    numerical value as in an integer.

--- ConselvaBudget/Models/ExpenseInvoice.cs
+++ ConselvaBudget/Models/ExpenseInvoice.cs
@@ -66,7 +66,8 @@ namespace ConselvaBudget.Models
         /// </summary>
         [Display(Name = "EXPENSE_INVOICE_CREDIT_CARD_ENDING")]
         [RegularExpression("([0-9][0-9][0-9][0-9])", ErrorMessage = "CREDIT_CARD_ENDING_FORMAT_INVALID")]
-        public int? CreditCardEnding { get; set; }
+        [StringLength(4)]
+        public string CreditCardEnding { get; set; }
 
         /// <summary>
         /// Only applicable if the payment method is credit card.
