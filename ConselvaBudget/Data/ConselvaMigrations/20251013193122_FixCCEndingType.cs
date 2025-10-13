4088f5b1eed929f8394667a8875230812fd4ab49
Felipe Bojorquez <felipe@febog.com>
2025-10-13 12:33:33 -0700

    Add CC info fix DB migration

---
+++ ConselvaBudget/Data/ConselvaMigrations/20251013193122_FixCCEndingType.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConselvaBudget.Data.ConselvaMigrations
+{
+    /// <inheritdoc />
+    public partial class FixCCEndingType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CreditCardEnding",
+                table: "ExpenseInvoices",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CreditCardEnding",
+                table: "ExpenseInvoices",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true);
+        }
+    }
+}
